ed6f793ffea09e61368a78d3abbc9ac39000862e
Ing. Acevedo <sa.projects@outlook.com>
2025-01-18 15:33:51 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,12 @@
+let bola;
+let velocidadX = 5;
+let velocidadY = 3;
+
+let jugadorY, pcY;
+const anchoRaqueta = 10;
+const altoRaqueta = 80;
+
+const margen = 20;
+const velocidadRaqueta = 5;
+const anchoCanvas = 800;
+const altoCanvas = 400;
